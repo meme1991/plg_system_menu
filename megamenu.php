ebeac8b14858fdad8e0119df878325c9fc6c5a63
Emanuele Cappato <e.kappato@icloud.com>
2025-11-17 11:00:40 +0100

    Update megamenu.php

--- megamenu.php
+++ megamenu.php
@@ -1,24 +1,38 @@
 <?php
-# @Author: SPEDI srl
-# @Date:   24-01-2018
-# @Email:  sviluppo@spedi.it
-# @Last modified by:   SPEDI srl
-# @Last modified time: 24-01-2018
-# @License: GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
-# @Copyright: Copyright (c) SPEDI srl
+/**
+ * @Author: SPEDI srl
+ * @Date:   24-01-2018
+ * @Last modified by:   SPEDI srl
+ * @Last modified time: 24-01-2018
+ * @License: GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
 
 defined('_JEXEC') or die;
 
-class plgSystemMegaMenu extends JPlugin
-{
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Form\Form;
 
-	function onContentPrepareForm($form, $data)
-	{
-		// extra option for menu item
-		if ($form->getName() == 'com_menus.item') {
-			$this->loadLanguage();
-			JForm::addFormPath(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'params');
-			$form->loadFile('spitem', true);
-		}
-	}
+class PlgSystemMegaMenu extends CMSPlugin
+{
+    /**
+     * Evento usato per aggiungere campi al form delle voci di menu
+     */
+    public function onContentPrepareForm(Form $form, $data)
+    {
+        // Verifico che sia il form delle voci di menu
+        if ($form->getName() !== 'com_menus.item') {
+            return;
+        }
+
+        // Carico la lingua del plugin
+        $this->loadLanguage();
+
+        // Aggiungo il percorso dei form XML
+        Form::addFormPath(__DIR__ . '/params');
+
+        // Carico il file spitem.xml
+        $form->loadFile('spitem', true);
+
+        return true;
+    }
 }
